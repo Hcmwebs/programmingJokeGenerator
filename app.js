f1de02b6161f1ae5ae14fc7ae38a84fab9eca4bb
Hezekia Chege <zeekprojectos@gmail.com>
2021-02-10 10:23:25 +1300

    updated script file clearing the comments
    Signed-off-by: Hezekia Chege <zeekprojectos@gmail.com>

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ generateJokes()
 async function generateJokes(){
   const res = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
   const data = await res.json();
-  // const resDev = await fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
-  // const dataDev = await resDev.json();
-  // console.log(data)
-  // console.log(data.joke)
-  // console.log(data.delivery)
   let joke = ''
   if(data.joke == undefined ){
     joke = `${data.setup} <br/> ${data.delivery}`
